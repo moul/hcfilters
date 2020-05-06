951c521c5984df1b171fe03af7106d5b37928210
Manfred Touron <94029+moul@users.noreply.github.com>
2020-05-06 21:59:56 +0200

    fix: update godoc

--- doc.go
+++ doc.go
@@ -1,6 +1,8 @@
 // Copyright © 2020 Manfred Touron <manfred.life>
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
+// Package hcfilters provides filtering middlewares for github.com/gregjones/httpcache.
+//
 // message from the author:
 //	+--------------------------------------------------------------+
 //	| * * * ░░░░░░░░░░░░░░░░░░░░  Hello  ░░░░░░░░░░░░░░░░░░░░░░░░░░|
@@ -26,4 +28,25 @@
 //	|/  |       |  \    \ /                    /  ' \/ _ \/ // / / |
 //	||  |       |  |    | |                   /_/_/_/\___/\_,_/_/  |
 //	+--------------------------------------------------------------+
+//
+// This is how to wrap an existing backend:
+//
+// before:
+//
+//     client := &http.Client{
+//         Transport: httpcache.NewTransport(
+//	       diskcache.New(diskcachePath),
+//         ),
+//     }
+//
+// after:
+//
+//     client := &http.Client{
+//         Transport: httpcache.NewTransport(
+//             hcfilters.MaxSize( // skip caching results > 2Mb
+//                 diskcache.New(diskcachePath),
+//                 2*1024*1024,
+//             ),
+//         ),
+//     }
 package hcfilters
