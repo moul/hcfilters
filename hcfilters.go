9eb4e320dddded5e73b49db71ee49f223f27a78c
Manfred Touron <94029+moul@users.noreply.github.com>
2020-05-06 20:41:01 +0200

    feat: MaxSize filter

---
+++ hcfilters.go
@@ -0,0 +1,38 @@
+package hcfilters
+
+func MaxSize(next Cache, limit int) Cache {
+	return maxSizeFilter{
+		next:  next,
+		limit: limit,
+	}
+}
+
+type maxSizeFilter struct {
+	next  Cache
+	limit int
+}
+
+func (f maxSizeFilter) Get(key string) (responseBytes []byte, ok bool) {
+	return f.next.Get(key)
+}
+
+func (f maxSizeFilter) Delete(key string) {
+	f.next.Delete(key)
+}
+
+func (f maxSizeFilter) Set(key string, responseBytes []byte) {
+	if len(responseBytes) < f.limit {
+		f.next.Set(key, responseBytes)
+	}
+}
+
+// Cache is a clone of the httpcache.Cache struct to avoid having a dependency on the original repo
+type Cache interface {
+	// Get returns the []byte representation of a cached response and a bool
+	// set to true if the value isn't empty
+	Get(key string) (responseBytes []byte, ok bool)
+	// Set stores the []byte representation of a response against a key
+	Set(key string, responseBytes []byte)
+	// Delete removes the value associated with the key
+	Delete(key string)
+}
